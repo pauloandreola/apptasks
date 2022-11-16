0654efa00935012588c957c7135d544373ea1cf3
Paulo Andreola <pauloandreola@gmail.com>
2022-11-16 16:49:50 -0300

    feat: user routes created

---
+++ src/shared/routes/users.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+// import { CreateUserController } from "../../modules/users/useCases/createUser/createUserController";
+// import { UpdateUserController } from "../app/modules/accounts/useCases/updateUser/UpdateUserController";
+
+export const usersRoutes = Router();
+
+usersRoutes.get("/",)
+
+// usersRoutes.patch("/", updateUserController.handle);
+
+// usersRoutes.post("/", (request, response) => CreateUserController.handle(request, response));
