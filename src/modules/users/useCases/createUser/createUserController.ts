94048397ce1259d383ee10bcd9fea674b03170c3
Paulo Andreola <pauloandreola@gmail.com>
2022-11-16 17:13:35 -0300

    feat:  user create usecase files

---
+++ src/modules/users/useCases/createUser/createUserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { CreateUserUseCase } from "./createUserUseCase";
+
+
+export class CreateUserController {
+//   constructor(private createUserUseCase: CreateUserUseCase) {}
+
+//     async handle(request: Request, response: Response): Promise<Response>{
+//         try {
+//            const { name, email, password } = request.body;
+//            await this.createUserUseCase.execute({ name, email, password });
+//            return response.json("OK").status(200);
+//         } catch (error) {
+//             return response.status(400).json(error);
+//         }
+//     }
+}
---
+++ src/modules/users/useCases/createUser/createUserUseCase.ts
@@ -0,0 +1,21 @@
+import { IUserDTO } from "../../../dtos/IUserDTO";
+import { UsersRepository } from "../../infra/repositories/implementations/usersRepository";
+
+
+export class CreateUserUseCase {
+  // constructor(private usersRepository: UsersRepository){}
+
+  // async execute({name, email, password}:IUserDTO): Promise<void>{
+
+  //        const userExist = await this.usersRepository.findByEmail(email);
+
+  //         if(userExist){
+  //             throw new Error("E-mail already exist");
+  //         }
+
+  //         position  = position[0].toUpperCase() + position.substring(1) as any;
+
+  //        await this.usersRepository.addPlayer({name, email, password});
+      
+  // }
+}
