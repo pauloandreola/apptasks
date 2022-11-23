15935de8655fe214edb6f784186a61cf51c5d908
Paulo Andreola <pauloandreola@gmail.com>
2022-11-23 14:56:26 -0300

    refactor: Optimized files and folders clean code

--- src/modules/users/useCases/createUser/index.ts
+++
@@ -1,8 +0,0 @@
-import { UsersRepository } from "../../infra/repositories/implementations/usersRepository";
-import { CreateUserController } from "./createUserController";
-import { CreateUserUseCase } from "./createUserUseCase";
-
-
-const usersRepository = new UsersRepository();
-// const createUserUseCase = new CreateUserUseCase(usersRepository);
-// export const createUserController = new CreateUserController(createUserUseCase);
