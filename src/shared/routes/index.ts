47457297e1cf487ef76f167eb133b1f9ad06a4a3
Paulo Andreola <pauloandreola@gmail.com>
2022-11-29 10:37:48 -0300

    feat: method create table in docker db

--- src/shared/routes/index.ts
+++ src/shared/routes/index.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
+import { TasksRepository } from '../../modules/tasks/infra/repositories/implementations/tasksRepository';
+import { UsersRepository } from '../../modules/users/infra/repositories/implementations/usersRepository';
 import { tasksRoutes } from './tasks.routes';
 import { usersRoutes } from './users.routes';
 
 export const router = Router();
 
-router.use('/users', usersRoutes);
+const createTableTaskRepository = new TasksRepository();
+createTableTaskRepository.createTaskTable();
+
+const createTableUserRepository = new UsersRepository();
+createTableUserRepository.createUserTable();
+
 router.use('/tasks', tasksRoutes);
+router.use('/users', usersRoutes);
