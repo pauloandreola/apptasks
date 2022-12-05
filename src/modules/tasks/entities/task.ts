0f34386e7dc887d85a8552453ff124e3db2b8d43
Paulo Andreola <pauloandreola@gmail.com>
2022-12-05 14:41:54 -0300

    refactor: included project object

--- src/modules/tasks/entities/task.ts
+++ src/modules/tasks/entities/task.ts
@@ -1,19 +1,23 @@
+import { v4 as uuidV4 } from 'uuid';
+
 export class Task {
   'taskId': string;
+  'project': string;
   'task': string;
   'created_at': Date;
   'updated_at': Date;
-  'start_date': Date;
   'end_date': Date;
   'total': number;
 
-  constructor(taskId: string, task: string, created_at: Date, updated_at: Date, start_date: Date, end_date: Date, total: number) {
+  constructor(taskId: string, project: string, task: string, created_at: Date, updated_at: Date, end_date: Date, total: number) {
+
     taskId: taskId;
+    project: project;
     task: task;
     created_at: created_at;
     updated_at: updated_at;
-    start_date: start_date;
     end_date: end_date;
     total: total;
+
   }
 }
