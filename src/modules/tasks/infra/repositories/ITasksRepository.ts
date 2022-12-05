854cde1c49d9f06ea5d13a438e446180ba352ab7
Paulo Andreola <pauloandreola@gmail.com>
2022-12-05 14:42:43 -0300

    refactor: included object in addtask method

--- src/modules/tasks/infra/repositories/ITasksRepository.ts
+++ src/modules/tasks/infra/repositories/ITasksRepository.ts
@@ -2,7 +2,7 @@ import { ITaskDTO } from "../../../dtos/ITaskDTO";
 import { Task } from "../../entities/task";
 
 export interface ITasksRepository {
-  addTask(data: ITaskDTO): Promise<void>;
+  addTask({taskId, project, task, created_at, updated_at, end_at, total}: ITaskDTO): Promise<void>;
   createTaskTable(): Promise<void>;
   findByTask(task: string): Promise<Task>;
   findById(id: string): Promise<Task>;
