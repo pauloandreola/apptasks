7744ae15f2393e129d4115d4d9aa17e090f9e170
Paulo Andreola <pauloandreola@gmail.com>
2022-12-06 08:23:20 -0300

    refactor: fix ip in host field

--- src/shared/infra/services/db.ts
+++ src/shared/infra/services/db.ts
@@ -1,7 +1,7 @@
 import mysql  from "mysql2";
 
 export const conn = mysql.createConnection({
-  host: "localhost",
+  host: '172.22.0.2',
   user: "root",
   password: "root",
   database: "apptasks"
