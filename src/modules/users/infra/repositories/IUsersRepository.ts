99d77513714ae01470e0fee94cad3c9c3d8939d6
Paulo Andreola <pauloandreola@gmail.com>
2022-12-08 21:10:44 -0300

    refactor: adduser adjust

--- src/modules/users/infra/repositories/IUsersRepository.ts
+++ src/modules/users/infra/repositories/IUsersRepository.ts
@@ -2,7 +2,7 @@ import { IUserDTO } from '../../../dtos/IUserDTO';
 import { User } from '../../entities/user';
 
 export interface IUsersRepository {
-  addUser({userId, name, email, admin, password, avatar, department, created_at, updated_at}: IUserDTO): Promise<void>;
+  addUser(data: IUserDTO): Promise<void>;
   createUserTable(): Promise<void>;
   findByEmail(email: string): Promise<User>;
   findById(id: string): Promise<User>;
