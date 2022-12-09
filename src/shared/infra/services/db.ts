b0d00b899ccc7763cffa1d4073cfb89bae50fca2
Paulo Andreola <pauloandreola@gmail.com>
2022-12-08 21:16:25 -0300

    fix: host adjust

--- src/shared/infra/services/db.ts
+++ src/shared/infra/services/db.ts
@@ -1,7 +1,7 @@
 import mysql  from "mysql2";
 
 export const conn = mysql.createConnection({
-  host: '172.22.0.2',
+  host: 'localhost',
   user: "root",
   password: "root",
   database: "apptasks"
