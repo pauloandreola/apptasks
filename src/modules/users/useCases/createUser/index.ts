60eeeaed5bc7ec81867775a88c06d39b26c6feca
Paulo Andreola <pauloandreola@gmail.com>
2022-12-15 01:48:40 -0300

    refactor: clear blank space

--- src/modules/users/useCases/createUser/index.ts
+++ src/modules/users/useCases/createUser/index.ts
@@ -2,9 +2,6 @@ import { UsersRepository } from "../../infra/repositories/implementations/usersR
 import { CreateUserController } from "./createUserController";
 import { CreateUserUseCase } from "./createUserUseCase";
 
-
 const usersRepository = new UsersRepository();
 const createUserUseCase = new CreateUserUseCase(usersRepository);
-const createUserController = new CreateUserController(createUserUseCase);
-
-export { createUserController }
+export const createUserController = new CreateUserController(createUserUseCase);
